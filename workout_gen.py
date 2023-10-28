24e5da95f12894b9b75fd016856c92a66523529b
Steven Korevaar <stevenkorevaar@gmail.com>
2023-10-28 15:37:32 +1100

    Added json support

--- workout_gen.py
+++ workout_gen.py
@@ -2,6 +2,11 @@
 import random
 import math
 
+class Exercise():
+    def __init__(self, body_parts, rep_ranges=["1..3", "4..6", "7..9", "10..12"]):
+        self.body_parts = body_parts
+        self.rep_ranges = rep_ranges
+
 body_part_reqs = [
     "chest",
     "triceps",
@@ -20,34 +25,30 @@ body_part_reqs = [
     "glutes"
 ]
 
+
 major_muscle_groups = {
     "push": ["chest", "triceps", "front deltoids"],
     "pull": ["upper back", "lower back", "lats"],
     "legs": ["quads", "hamstrings", "glutes"]
 }
 
-exercises = {
-    "bench press": ["chest", "triceps", "front deltoids"],
-    "overhead press": {"triceps", "front deltoids", "side deltoids"},
-    "tricep extensions": {"triceps"},
-    "chest flies": {"chest"},
+import json
+def read_exercises(filepath):
+    exercises = {}
+    f = json.load(open(filepath))
+    # print(f)
+    for exe in f["exercises"]:
+        for e in exe:
+            # print(e)
+            # print(exe[e])
+            exercises[e] = Exercise(exe[e][0]['body_parts'], exe[e][0]['rep_ranges'])
+    return exercises
 
-    "squat": {"quads", "hamstrings","glutes"},
+exercises = read_exercises("exercise_list.json")
+# print(exercises)
+# print(list(exercises.keys()))
 
-    "deadlift": {"lower back", "hamstrings", "quads", "glutes", "upper back", "lats"},
-
-    
-    "pull up": {"lats", "upper back", "biceps"},
-    "barbell row": {"lats", "upper back", "lower back", "rear deltoids"},
-    "dumbbell row": {"lats", "upper back", "lower back", "rear deltoids"},
-
-    "bicep curl": {"biceps"},
-    "chin up": {"biceps", "lats"},
-
-
-}
-
-def check_reqs(hit, chosen_exercises):
+def check_reqs(hit):
     for body_part in body_part_reqs: 
         if hit[body_part] <= 0: return False
     return True
@@ -57,17 +58,17 @@ def make_workout():
     chosen_exercises = []
     for body_part in body_part_reqs: hit[body_part] = 0
 
-    while not check_reqs(hit, chosen_exercises):
+    while not check_reqs(hit):
         chosen_exercise = random.choice(list(exercises.keys()))
         if chosen_exercise not in chosen_exercises:
             chosen_exercises.append(chosen_exercise)
             # print(chosen_exercise)
             # print(exercises[chosen_exercise])
-            for bp in exercises[chosen_exercise]: hit[bp] += 1
+            for bp in exercises[chosen_exercise].body_parts: hit[bp] += 1
         # else:
             # print("Already done", chosen_exercise)
     
-    return chosen_exercises
+    return chosen_exercises, hit
 
 def break_into_sessions(exercise_list, num_sessions):
     assert num_sessions <= len(exercise_list)
@@ -88,9 +89,12 @@ def break_into_sessions(exercise_list, num_sessions):
 
     return sessions
 
-exercise_list = make_workout()
+exercise_list, body_parts_hit = make_workout()
 print("\nChosen Exercises:")
 print(exercise_list)
+
+print("\nBody parts hit: ")
+print(body_parts_hit)
 
 sessions = break_into_sessions(exercise_list, 4)
 
